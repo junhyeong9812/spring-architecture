325ea59eb174691e9f8567299f3f158dfc83680f
junhyeong9812 <pickjog@gmail.com>
2026-03-07 20:33:24 +0900

    chore: 초기 구성

---
+++ src/main/java/com/shoptracker/ShopTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.shoptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopTrackerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/shoptracker/ShopTrackerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.shoptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
