2d6049f34b332f5df67d2d03f03e98feed7e927b
junhyeong9812 <pickjog@gmail.com>
2026-04-13 20:32:10 +0900

    feat: orderStatus 값 객체 추가

--- src/main/java/com/shoptracker/orders/domain/model/OrderStatus.java
+++ src/main/java/com/shoptracker/orders/domain/model/OrderStatus.java
@@ -1,4 +1,15 @@
 package com.shoptracker.orders.domain.model;
 
 public enum OrderStatus {
+    CREATED, PAYMENT_PENDING, PAID, SHIPPING, DELIVERED, CANCELLED;
+
+    public boolean canTransitionTo(OrderStatus target) {
+        return switch (this) {
+            case CREATED -> target == PAYMENT_PENDING || target == CANCELLED;
+            case PAYMENT_PENDING -> target == PAID || target == CANCELLED;
+            case PAID -> target == SHIPPING;
+            case SHIPPING -> target == DELIVERED;
+            case DELIVERED, CANCELLED -> false;
+        };
+    }
 }
