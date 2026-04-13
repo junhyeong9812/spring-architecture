c93b78a42dd2e37ec0beea20c2328531c8f2aafc
junhyeong9812 <pickjog@gmail.com>
2026-04-13 18:06:52 +0900

    feat: 구독 리포지토리 어뎁터 추가

--- src/main/java/com/shoptracker/subscription/adapter/outbound/persistence/SubscriptionPersistenceAdapter.java
+++ src/main/java/com/shoptracker/subscription/adapter/outbound/persistence/SubscriptionPersistenceAdapter.java
@@ -1,4 +1,32 @@
 package com.shoptracker.subscription.adapter.outbound.persistence;
 
-public class SubscriptionPersistenceAdapter {
+import com.shoptracker.subscription.domain.model.Subscription;
+import com.shoptracker.subscription.domain.model.SubscriptionId;
+import com.shoptracker.subscription.domain.port.outbound.SubscriptionRepository;
+
+import java.util.Optional;
+
+public class SubscriptionPersistenceAdapter implements SubscriptionRepository {
+    private final SpringDataSubscriptionRepository jpaRepository;
+
+    public SubscriptionPersistenceAdapter(SpringDataSubscriptionRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public void save(Subscription subscription) {
+        jpaRepository.save(SubscriptionMapper.toJpa(subscription));
+    }
+
+    @Override
+    public Optional<Subscription> findById(SubscriptionId id) {
+        return jpaRepository.findById(id.value())
+                .map(SubscriptionMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Subscription> findActiveByCustomer(String customerName) {
+        return jpaRepository.findActiveByCustomerName(customerName)
+                .map(SubscriptionMapper::toDomain);
+    }
 }
