c43b6c5af69900fa1bc6d1c0a85bd89790d80aa1
junhyeong9812 <pickjog@gmail.com>
2026-04-14 08:59:07 +0900

    feat: roderRepository 추가

--- src/main/java/com/shoptracker/orders/domain/port/outbound/OrderRepository.java
+++ src/main/java/com/shoptracker/orders/domain/port/outbound/OrderRepository.java
@@ -1,4 +1,15 @@
 package com.shoptracker.orders.domain.port.outbound;
 
+import com.shoptracker.orders.domain.model.Order;
+import com.shoptracker.orders.domain.model.OrderId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface OrderRepository {
+    void save(Order order);
+    Optional<Order> findById(OrderId id);
+    Page<Order> findAllByCustomerName(String customerName, Pageable pageable);
+    Page<Order> findAll(Pageable pageable);
 }
