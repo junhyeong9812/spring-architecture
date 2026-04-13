2988a1c1d23d4b4d00cc87e40eeafae83b544ada
junhyeong9812 <pickjog@gmail.com>
2026-04-13 18:03:40 +0900

    feat: 구독 엔티티 도메인 매퍼 추가

--- src/main/java/com/shoptracker/subscription/adapter/outbound/persistence/SubscriptionMapper.java
+++ src/main/java/com/shoptracker/subscription/adapter/outbound/persistence/SubscriptionMapper.java
@@ -1,4 +1,31 @@
 package com.shoptracker.subscription.adapter.outbound.persistence;
 
+import com.shoptracker.subscription.domain.model.Subscription;
+import com.shoptracker.subscription.domain.model.SubscriptionId;
+import com.shoptracker.subscription.domain.model.SubscriptionStatus;
+import com.shoptracker.subscription.domain.model.SubscriptionTier;
+
 public class SubscriptionMapper {
+
+    public static Subscription toDomain(SubscriptionJpaEntity entity) {
+        return new Subscription(
+                new SubscriptionId(entity.getId()),
+                entity.getCustomerName(),
+                SubscriptionTier.fromString(entity.getTier()),
+                SubscriptionStatus.valueOf(entity.getStatus()),
+                entity.getStartedAt(),
+                entity.getExpiresAt()
+        );
+    }
+
+    public static SubscriptionJpaEntity toJpa(Subscription domain) {
+        SubscriptionJpaEntity entity = new SubscriptionJpaEntity();
+        entity.setId(domain.getId().value());
+        entity.setCustomerName(domain.getCustomerName());
+        entity.setTier(domain.getTier().getValue());
+        entity.setStatus(domain.getStatus().name());
+        entity.setStartedAt(domain.getStartedAt());
+        entity.setExpiresAt(domain.getExpiresAt());
+        return entity;
+    }
 }
