38968d9c8adc04cac5380ada8c7502092525abaf
junhyeong9812 <pickjog@gmail.com>
2026-04-14 18:28:42 +0900

    feat: 주문 생성 서비스 구현체 추가

--- src/main/java/com/shoptracker/orders/application/service/OrderCommandService.java
+++ src/main/java/com/shoptracker/orders/application/service/OrderCommandService.java
@@ -1,4 +1,43 @@
 package com.shoptracker.orders.application.service;
 
-public class OrderCommandService {
+import com.shoptracker.orders.adapter.inbound.web.OrderItemRequest;
+import com.shoptracker.orders.application.port.inbound.CreateOrderUseCase;
+import com.shoptracker.orders.domain.model.Money;
+import com.shoptracker.orders.domain.model.Order;
+import com.shoptracker.orders.domain.model.OrderItem;
+import com.shoptracker.orders.domain.port.outbound.OrderRepository;
+import com.shoptracker.shared.events.OrderCreatedEvent;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class OrderCommandService implements CreateOrderUseCase {
+    private final OrderRepository orderRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Override
+    public UUID createOrder(String customerName, List<OrderItemRequest> items) {
+        List<OrderItem> orderItems = items.stream()
+                .map(i -> new OrderItem(i.productName(), i.quantity(),new Money(i.unitPrice())))
+                .toList();
+
+        Order order = Order.create(customerName, orderItems);
+        orderRepository.save(order);
+
+        eventPublisher.publishEvent(new OrderCreatedEvent(
+                order.getId().value(),
+                customerName,
+                order.getTotalAmount().amount(),
+                items.size(),
+                Instant.now()
+        ));
+
+        return order.getId().value();
+    }
 }
