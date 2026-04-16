8afaaf66de9771fbaf6931991ffc0d403a3c2bec
junhyeong9812 <pickjog@gmail.com>
2026-04-17 08:10:19 +0900

    test: 모듈 경계 테스트

--- src/test/java/com/shoptracker/ModuleStructureTest.java
+++ src/test/java/com/shoptracker/ModuleStructureTest.java
@@ -1,4 +1,22 @@
 package com.shoptracker;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
 public class ModuleStructureTest {
+
+    @Test
+    void verifyModuleStructure() {
+        ApplicationModules modules = ApplicationModules.of(ShopTrackerApplication.class);
+        modules.verify();
+    }
+
+    @Test
+    void generateModuleDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ShopTrackerApplication.class);
+        new Documenter(modules)
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
+    }
 }
