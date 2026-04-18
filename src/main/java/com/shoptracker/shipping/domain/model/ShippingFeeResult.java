2580a592eca0d6bd94941eb6d63fcca9a2447ba6
junhyeong9812 <pickjog@gmail.com>
2026-04-19 08:53:44 +0900

    feat: 배송 비용 결과 객체 추가

--- src/main/java/com/shoptracker/shipping/domain/model/ShippingFeeResult.java
+++ src/main/java/com/shoptracker/shipping/domain/model/ShippingFeeResult.java
@@ -1,4 +1,10 @@
 package com.shoptracker.shipping.domain.model;
 
-public record ShippingFeeResult() {
-}
+import com.shoptracker.orders.domain.model.Money;
+
+public record ShippingFeeResult(
+        Money fee,
+        Money originalFee,
+        String discountType,    // "none", "basic_half", "premium_free"
+        String reason
+) {}
