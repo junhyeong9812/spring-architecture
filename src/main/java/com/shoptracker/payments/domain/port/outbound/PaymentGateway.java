e9bf5ffb52beed3874dac63b8d49b84faf96e0fd
junhyeong9812 <pickjog@gmail.com>
2026-04-18 20:06:52 +0900

    feat: 결제 게이트웨이 인터페이스 추가

--- src/main/java/com/shoptracker/payments/domain/port/outbound/PaymentGateway.java
+++ src/main/java/com/shoptracker/payments/domain/port/outbound/PaymentGateway.java
@@ -1,4 +1,8 @@
 package com.shoptracker.payments.domain.port.outbound;
 
+import com.shoptracker.payments.domain.model.GatewayResult;
+import com.shoptracker.payments.domain.model.Payment;
+
 public interface PaymentGateway {
+    GatewayResult process(Payment payment);
 }
