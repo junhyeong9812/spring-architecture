b04f47147ec20639d507414e3864134a8556bf06
junhyeong9812 <pickjog@gmail.com>
2026-04-18 23:08:40 +0900

    feat: 결제 프로세스 유즈 케이스 추가

--- src/main/java/com/shoptracker/payments/application/port/inbound/ProcessPaymentUseCase.java
+++ src/main/java/com/shoptracker/payments/application/port/inbound/ProcessPaymentUseCase.java
@@ -1,4 +1,8 @@
 package com.shoptracker.payments.application.port.inbound;
 
+import com.shoptracker.payments.application.command.ProcessPaymentCommand;
+import java.util.UUID;
+
 public interface ProcessPaymentUseCase {
+    UUID processPayment(ProcessPaymentCommand command);
 }
