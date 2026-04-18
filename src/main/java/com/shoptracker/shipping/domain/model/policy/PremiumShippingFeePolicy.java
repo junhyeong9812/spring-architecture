21a68b1911ebd71dcf0a41561ad454fb0b92e493
junhyeong9812 <pickjog@gmail.com>
2026-04-19 08:53:30 +0900

    feat: 배송 비용 정책 프리미엄객체 추가

--- src/main/java/com/shoptracker/shipping/domain/model/policy/PremiumShippingFeePolicy.java
+++ src/main/java/com/shoptracker/shipping/domain/model/policy/PremiumShippingFeePolicy.java
@@ -1,4 +1,16 @@
 package com.shoptracker.shipping.domain.model.policy;
 
-public class PremiumShippingFeePolicy {
+import com.shoptracker.orders.domain.model.Money;
+import com.shoptracker.shipping.domain.model.ShippingFeeResult;
+import com.shoptracker.shipping.domain.port.outbound.ShippingFeePolicy;
+import java.math.BigDecimal;
+
+public class PremiumShippingFeePolicy implements ShippingFeePolicy {
+    private static final Money BASE_FEE = new Money(new BigDecimal("3000"));
+
+    @Override
+    public ShippingFeeResult calculateFee(Money orderAmount) {
+        return new ShippingFeeResult(Money.ZERO, BASE_FEE, "premium_free",
+                "Premium 구독 무료배송");
+    }
 }
