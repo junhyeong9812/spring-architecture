bab1c923bba5132796da38d7cee0a0d2aa690942
junhyeong9812 <pickjog@gmail.com>
2026-04-19 08:52:45 +0900

    feat: 배송 아이디 객체 추가

--- src/main/java/com/shoptracker/shipping/domain/model/ShipmentId.java
+++ src/main/java/com/shoptracker/shipping/domain/model/ShipmentId.java
@@ -1,4 +1,5 @@
 package com.shoptracker.shipping.domain.model;
 
-public record ShipmentId() {
+public record ShipmentId(java.util.UUID value) {
+    public static ShipmentId generate() { return new ShipmentId(java.util.UUID.randomUUID()); }
 }
