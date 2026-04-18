2ec36226a0e2ebf6332998dc1de3685c6ccc6f9b
junhyeong9812 <pickjog@gmail.com>
2026-04-19 08:53:07 +0900

    feat: 배송 비용 정책 객체 추가

--- src/main/java/com/shoptracker/shipping/domain/port/outbound/ShippingFeePolicy.java
+++ src/main/java/com/shoptracker/shipping/domain/port/outbound/ShippingFeePolicy.java
@@ -1,4 +1,8 @@
 package com.shoptracker.shipping.domain.port.outbound;
 
+import com.shoptracker.orders.domain.model.Money;
+import com.shoptracker.shipping.domain.model.ShippingFeeResult;
+
 public interface ShippingFeePolicy {
+    ShippingFeeResult calculateFee(Money orderAmount);
 }
