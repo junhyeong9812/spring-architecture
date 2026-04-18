3462bce52d5f9f6b305049c5880c2c59ffc5d483
junhyeong9812 <pickjog@gmail.com>
2026-04-18 20:05:29 +0900

    feat: 할인 인터페이스 추가

--- src/main/java/com/shoptracker/payments/domain/port/outbound/DiscountPolicy.java
+++ src/main/java/com/shoptracker/payments/domain/port/outbound/DiscountPolicy.java
@@ -1,4 +1,8 @@
 package com.shoptracker.payments.domain.port.outbound;
 
+import com.shoptracker.orders.domain.model.Money;
+import com.shoptracker.payments.domain.model.DiscountResult;
+
 public interface DiscountPolicy {
+    DiscountResult calculateDiscount(Money amount);
 }
