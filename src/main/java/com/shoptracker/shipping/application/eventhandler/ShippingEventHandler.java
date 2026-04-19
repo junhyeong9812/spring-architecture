d2cab7bf2fda4d07cf72025a5b562573a5acaaf6
junhyeong9812 <pickjog@gmail.com>
2026-04-19 20:23:07 +0900

    feat: 배송 이벤트 핸들러 추가

--- src/main/java/com/shoptracker/shipping/application/eventhandler/ShippingEventHandler.java
+++ src/main/java/com/shoptracker/shipping/application/eventhandler/ShippingEventHandler.java
@@ -1,4 +1,24 @@
 package com.shoptracker.shipping.application.eventhandler;
 
+import com.shoptracker.shared.events.PaymentApprovedEvent;
+import com.shoptracker.shipping.application.service.ShipmentCommandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ShippingEventHandler {
+    private static final Logger log = LoggerFactory.getLogger(ShippingEventHandler.class);
+    private final ShipmentCommandService shipmentCommandService;
+
+    public ShippingEventHandler(ShipmentCommandService shipmentCommandService) {
+        this.shipmentCommandService = shipmentCommandService;
+    }
+
+    @ApplicationModuleListener
+    public void onPaymentApproved(PaymentApprovedEvent event) {
+        log.info("Payment approved for order {}, creating shipment...", event.orderId());
+        shipmentCommandService.createShipment(event.orderId(), event.finalAmount());
+    }
 }
