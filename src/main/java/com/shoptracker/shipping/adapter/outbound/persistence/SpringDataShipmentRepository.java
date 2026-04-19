b237e616d993167318906fe60819cf85586c0ee5
junhyeong9812 <pickjog@gmail.com>
2026-04-19 20:25:33 +0900

    feat: 배송 리포지토리 객체 추가

--- src/main/java/com/shoptracker/shipping/adapter/outbound/persistence/SpringDataShipmentRepository.java
+++ src/main/java/com/shoptracker/shipping/adapter/outbound/persistence/SpringDataShipmentRepository.java
@@ -1,4 +1,9 @@
 package com.shoptracker.shipping.adapter.outbound.persistence;
 
-public interface SpringDataShipmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SpringDataShipmentRepository extends JpaRepository<ShipmentJpaEntity, UUID> {
+    Optional<ShipmentJpaEntity> findByOrderId(UUID orderId);
 }
