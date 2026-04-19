0c68b1bd6bf50449e770d10ed5c508bd8fc5564a
junhyeong9812 <pickjog@gmail.com>
2026-04-19 20:25:05 +0900

    feat: 배송 퍼시스턴스 어댑터 추가

--- src/main/java/com/shoptracker/shipping/adapter/outbound/persistence/ShipmentPersistenceAdapter.java
+++ src/main/java/com/shoptracker/shipping/adapter/outbound/persistence/ShipmentPersistenceAdapter.java
@@ -1,4 +1,35 @@
 package com.shoptracker.shipping.adapter.outbound.persistence;
 
-public class ShipmentPersistenceAdapter {
+import com.shoptracker.shipping.domain.model.Shipment;
+import com.shoptracker.shipping.domain.model.ShipmentId;
+import com.shoptracker.shipping.domain.port.outbound.ShipmentRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class ShipmentPersistenceAdapter implements ShipmentRepository{
+
+    private final SpringDataShipmentRepository jpaRepository;
+
+    public ShipmentPersistenceAdapter(SpringDataShipmentRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public void save(Shipment shipment) {
+        jpaRepository.save(ShipmentMapper.toJpa(shipment));
+    }
+
+    @Override
+    public Optional<Shipment> findById(ShipmentId id) {
+        return jpaRepository.findById(id.value())
+                .map(ShipmentMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Shipment> findByOrderId(UUID orderId) {
+        return jpaRepository.findByOrderId(orderId)
+                .map(ShipmentMapper::toDomain);
+    }
 }
