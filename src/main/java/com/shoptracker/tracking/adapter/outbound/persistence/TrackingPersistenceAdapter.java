16cfc655a169d5a4a63234bf4cc0ef99bd46d7f9
junhyeong9812 <pickjog@gmail.com>
2026-04-22 08:17:30 +0900

    feat: tracking 어뎁터 추가

--- src/main/java/com/shoptracker/tracking/adapter/outbound/persistence/TrackingPersistenceAdapter.java
+++ src/main/java/com/shoptracker/tracking/adapter/outbound/persistence/TrackingPersistenceAdapter.java
@@ -1,4 +1,28 @@
 package com.shoptracker.tracking.adapter.outbound.persistence;
 
-public class TrackingPersistenceAdapter {
+import com.shoptracker.tracking.domain.model.OrderTracking;
+import com.shoptracker.tracking.domain.port.outbound.TrackingRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class TrackingPersistenceAdapter implements TrackingRepository {
+    private final SpringDataTrackingRepository jpaRepository;
+
+    public TrackingPersistenceAdapter(SpringDataTrackingRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public void save(OrderTracking tracking) {
+        jpaRepository.save(TrackingMapper.toJpa(tracking));
+    }
+
+    @Override
+    public Optional<OrderTracking> findByOrderId(UUID orderId) {
+        return jpaRepository.findByOrderId(orderId)
+                .map(TrackingMapper::toDomain);
+    }
 }
