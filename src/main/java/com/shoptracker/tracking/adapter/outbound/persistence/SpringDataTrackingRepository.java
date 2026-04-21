34d40dd2196e9a0e3b9c23d3939ead8db16f0ee6
junhyeong9812 <pickjog@gmail.com>
2026-04-22 08:17:39 +0900

    feat: tracking jpa 리포지토리 추가

--- src/main/java/com/shoptracker/tracking/adapter/outbound/persistence/SpringDataTrackingRepository.java
+++ src/main/java/com/shoptracker/tracking/adapter/outbound/persistence/SpringDataTrackingRepository.java
@@ -1,4 +1,11 @@
 package com.shoptracker.tracking.adapter.outbound.persistence;
 
-public interface SpringDataTrackingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SpringDataTrackingRepository extends JpaRepository<TrackingJpaEntity, UUID> {
+
+    Optional<TrackingJpaEntity> findByOrderId(UUID orderId);
 }
